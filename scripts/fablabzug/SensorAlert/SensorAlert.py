1c4211e50a6b53b776ddac5a966a7416797d269b
Nijo <n.scheibner@gmx.ch>
2020-04-30 21:53:11 +0200

    Fixed sql query of SensorAlert.py and email logic

--- scripts/fablabzug/SensorAlert/SensorAlert.py
+++ scripts/fablabzug/SensorAlert/SensorAlert.py
@@ -15,7 +15,7 @@ def read_current_environment_and_create_alert_message():
     # Get environment data from database
     conn = sqlite3.connect(config["database_path"])
     cursor = conn.cursor()
-    cursor.execute("SELECT state FROM app_environmentmodel LIMIT 1")
+    cursor.execute("SELECT state FROM app_environmentmodel ORDER BY timestamp DESC LIMIT 1")
     rows = cursor.fetchall()
 
     environmentJson = rows[0][0]
@@ -23,7 +23,7 @@ def read_current_environment_and_create_alert_message():
     sensorValues = environment["sensor"]["reported"]
 
     # Check if any value is outside configured range
-    sensorAlertString = "Einer oder mehrere Werte sind ausserhalb des idealen Bereichs:\n"
+    sensorAlertString = ""
     sensorConfiguration = config["sensors"]
 
     for sensor in sensorValues.items():
@@ -50,23 +50,27 @@ gmail_password = config["email_account"]["password"]
 sent_from = gmail_user
 to = config["email_recipients"]
 subject = 'Sensor Alarm'
-body = read_current_environment_and_create_alert_message();
 
-email_text = """\
-From: %s
-To: %s
-Subject: %s
+sensorString = read_current_environment_and_create_alert_message()
+if sensorString != '':
 
-%s
-""" % (sent_from, ", ".join(to), subject, body)
+    body = "Einer oder mehrere Werte sind ausserhalb des idealen Bereichs:\n" + sensorString
 
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(gmail_user, gmail_password)
-    server.sendmail(sent_from, to, email_text)
-    server.close()
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
 
-    print('Email sent!')
-except:
-    print('Something went wrong...')
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
