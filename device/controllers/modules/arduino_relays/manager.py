393e86ce7b1c78bfd92afa6a6696ec3c12795a79
Nijo <n.scheibner@gmx.ch>
2020-07-01 23:05:06 +0200

    Implemented a temperature tolerance of 1 Implemented a try time of 10 minutes Implemented a stay off time of 120 minutes

--- device/controllers/modules/arduino_relays/manager.py
+++ device/controllers/modules/arduino_relays/manager.py
@@ -1,4 +1,5 @@
 # Import standard python modules
+import datetime, time
 from typing import Optional, Tuple, Dict, Any
 
 # Import controller manager parent class
@@ -6,6 +7,12 @@ from device.controllers.classes.controller import manager, modes
 
 class ArduinoRelaysControllerManager(manager.ControllerManager):
     """Manages a controller with Arduino relay logic."""
+
+    cooling_turn_on_time = None;
+    cooling_try_minutes = 10;
+    cooling_retry_minutes = 120;
+    cooling_tolerance = 1;
+    cooling_stay_off = False;
 
     # --------------------------------------------------------------------------------------
     def __init__(self, *args: Any, **kwargs: Any) -> None:
@@ -57,7 +64,6 @@ class ArduinoRelaysControllerManager(manager.ControllerManager):
         value = self.state.get_environment_desired_actuator_value(
             self.negative_actuator_name
         )
-        self.logger.info("??????????? Setting negative actuator")
         if value != None:
             return float(value)
         return None
@@ -99,11 +105,30 @@ class ArduinoRelaysControllerManager(manager.ControllerManager):
                 self.logger.warning("sensor_value or desired_sensor_value is not a float.")
                 return
 
+            if self.cooling_stay_off == True :
+                if (datetime.datetime.now() - self.cooling_turn_on_time).total_seconds() / 60 >= self.cooling_retry_minutes :
+                    self.cooling_stay_off = False;
+                    self.cooling_turn_on_time = None;
+                    self.logger.warning("resumed normal operation")
+                else:
+                    self.logger.warning("still in stay off mode")
+                    return;
+
             # For now just slam between 0 and 100%.
-            if self.sensor_value > self.desired_sensor_value:
+            if self.sensor_value > self.desired_sensor_value + self.cooling_tolerance:
                 self.desired_negative_actuator_percent = 100.0
-            else:
+
+                if self.cooling_turn_on_time == None :
+                    self.cooling_turn_on_time = datetime.datetime.now()
+
+            elif self.sensor_value <= self.desired_sensor_value - self.cooling_tolerance:
                 self.desired_negative_actuator_percent = 0.0
+
+            # If cooling ran for cooling_try_minutes and desired value is still not reached then turn off and stay off for cooling_retry_minutes
+            if (datetime.datetime.now() - self.cooling_turn_on_time).total_seconds() / 60 >= self.cooling_try_minutes :
+                self.desired_negative_actuator_percent = 0.0
+                self.cooling_stay_off = True;
+                self.logger.warning(f"paused for {self.cooling_retry_minutes} minutes")
 
     # --------------------------------------------------------------------------------------
     def clear_reported_values(self) -> None:
